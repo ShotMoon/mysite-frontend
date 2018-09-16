5f8d083f2508871d7afd274540a4a46d0855a6c3
ShotMoon <alex.xu@perficient.com>
2018-09-16 15:19:37 +0800

    handle webpack picture problem

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -26,10 +26,15 @@ module.exports = {
         ]
       },
       {
-        // 图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
-        // 如下配置，将小于8192byte的图片转成base64码
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader?limit=8192&name=../images/[name].[ext]?[hash]',
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   },
